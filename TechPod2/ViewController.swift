7478e51e5594a3c92a4c1b1e430e0afbf8d905eb
Maho Misumi <maho.smile606@gmail.com>
2017-11-04 20:21:44 +0900

    https://github.com/Sanchan-Misumi/techpod2.git

--- TechPod2/ViewController.swift
+++ TechPod2/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    var songNameArray = [String]()
+    var fileNameArray = [String]()
+    var imageNameArray = [String]()
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.dataSource = self
+        songNameArray = ["カノン", "エリーゼのために", "G線状のマリア"]
+        fileNameArray = ["cannon", "elise", "aria"]
+        imageNameArray = ["Pachelbel.jpg","Beethoen.jpg","Bach.jpg"]
+        table.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました")
+        
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource: fileNameArray[indexPath.row], ofType: "mp3")!)
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        audioPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
